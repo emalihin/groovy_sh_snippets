a0db2030881f4aa7ea1cedfb3062e3d0a95a71f7
kp0ccobep <kp0ccobep@gmail.com>
2016-08-24 14:46:59 +0200

    Update force_deregister_consul_service.sh

--- force_deregister_consul_service.sh
+++ force_deregister_consul_service.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-
-
 SERVICE_NAME=$1
 
-
-
 SERVICE_ID=$(curl -X GET http://consul.somewhere.com:8500/v1/catalog/service/"$SERVICE_NAME" \
-
 | python -m json.tool \
-
 | grep ServiceID  \
-
 | awk '{gsub(/"/, ""); gsub(/,/, ""); print $2}')
-
-
 
 curl -X PUT http://consul.somewhere.com:8500/v1/agent/service/deregister/"$SERVICE_ID"
